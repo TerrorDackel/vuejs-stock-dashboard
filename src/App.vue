e1978e394a4fd4a241c489ca71a85e906e8b7d04
Jennifer Thomas <jenny.codes.007@gmail.com>
2025-10-28 10:06:31 +0100

    first component BaseCard created

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
-  <div id="app">
-
-  </div>
+  <BaseCard>
+    <h1>VueJS Stock Dashboard</h1>
+    <p>Willkommen.</p>
+  </BaseCard>
 </template>
 
 <script>
-
+import BaseCard from './components/BaseCard.vue';
 
 export default {
   name: 'App',
-  components: {
-    
+  components: { 
+    BaseCard
   }
 }
 </script>
@@ -18,17 +19,18 @@ export default {
 <style>
 
 body {
+  width: 100%;
   margin: 0;
 }
 
 #app {
   width: 100vw;
-  height: 100vh;
-  
+  min-height: 100vh;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  box-sizing: border-box;
+  padding: 100px;
   background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011F35 100%);
-
 }
 </style>
---
+++ src/components/BaseCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseCard'
+}
+</script>
+
+<style scoped>
+.card {
+  width: 100%;
+  padding: 24px 32px;
+  border-radius: 1rem;
+  background: #011F35;
+}
+</style>
