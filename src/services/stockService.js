b28007a239f1d9b0137ce314b7bf6eed8b5c0a7a
Jennifer Thomas <jenny.codes.007@gmail.com>
2025-10-28 16:05:08 +0100

    sorted the order of stockService

--- src/services/stockService.js
+++ src/services/stockService.js
@@ -1,10 +1,28 @@
 import axios from 'axios';
 
+const order = [
+  'Mar 21',
+  'Jun 21',
+  'Sep 21',
+  'Dec 21',
+  'Mar 22',
+  'Jun 22',
+  'Dec 22',
+  'Mar 23',
+  '""',
+  '3 Aug 23',
+  '2 Nov 23',
+  '1 Feb 24',
+  '2 Mai 24'
+];
+
 class StockService {
+
   constructor() {
     this.apiClient = axios.create({
       baseURL: 'https://sheetdb.io/api/v1/h383llrajzdr9'
     });
+
   }
 
   async fetchData(sheetName) {
@@ -16,6 +34,12 @@ class StockService {
       return [];
     }
   }
+
+  async getRevenue(sheetName) {
+    const data = await this.fetchData(sheetName)
+    return order.map(key => data[3][key]);
+  }
+
 }
 
 export const stockService = new StockService();
